841240042397a34fb68c3673e3418810e5b81e04
MunSeongUk <door6740@gmail.com>
2020-08-16 19:39:49 +0900

    11051 완료

---
+++ 11501/Main.java
@@ -0,0 +1,39 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+	private static Scanner sc;
+	
+	private static int N;
+	private static int K;
+
+	private static int[][] dp = new int[1001][1001];
+	
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		K = sc.nextInt();
+		
+		int answer = pascalTriangle(N, K) % 10007;
+		
+		System.out.println(answer);
+	}
+	
+	private static int pascalTriangle(int n, int r) {
+		if (n == r || r == 0) {
+			dp[n][r] = 1;
+			return dp[n][r];
+		} else if (r == 1) {
+			dp[n][r] = n;
+			return dp[n][r];
+		}
+		
+		
+		if (dp[n][r] == 0) {
+			dp[n][r] = pascalTriangle(n - 1, r - 1) % 10007 + pascalTriangle(n - 1, r) % 10007;
+		}
+		
+		return dp[n][r];
+	}
+}
---
+++ 11501/README.md
@@ -0,0 +1,3 @@
+11051 - 이항계수<br>
+======
+[https://www.acmicpc.net/problem/11051]
