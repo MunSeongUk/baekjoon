5e11f3f2a121decaf7f7f5cf4ced62ef9422e3d6
DESKTOP-BT06A7U\door6 <door6740@gmail.com>
2020-11-17 16:32:00 +0900

    1654 수정

--- 1654/Main.java
+++ 1654/Main.java
@@ -1,10 +1,3 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -12,27 +5,28 @@ public class Main {
 	private static long N, K;
 	private static long[] CABLES;
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		sc = new Scanner(System.in);
-//		sc = new Scanner(new File("input.txt"));
 		
 		N = sc.nextLong();
 		K = sc.nextLong();
 		CABLES = new long[10001];
 		
-		long max = 0;
+		long left = 1;
+		long right = 0;
 		
 		for (int i = 0; i < N; ++i) {
 			CABLES[i] = sc.nextLong();
-			max += CABLES[i];
+			
+			if (right < CABLES[i]) {
+				right = CABLES[i];
+			}
 		}
 		
-		long left = 0;
-		long right = max / K;
-		max = 0;
+		long max = 0;
 		
 		while (left <= right) {
-			long mid = (left + right) / 2 == 0 ? 1 : (left + right) / 2;
+			long mid = (left + right) / 2;
 			long answer = 0;
 			
 			for (int i = 0; i < N; ++i) {
